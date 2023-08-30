5df4ad67b6bea783a7f92f0a5a4b407c445570b5
KiKamble <kiran.kamble1710@gmail.com>
2023-08-30 08:17:00 +0530

    code change

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,7 +7,9 @@ package com.mycompany.app;
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {int a=20;
+int b=30;
+int c=a+b;
+        System.out.println( "Hello World!"+ c );
     }
 }
